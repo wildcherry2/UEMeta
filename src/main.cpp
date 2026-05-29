4c89c31d5321317ab710317723d36913d892ad24
wildcherry2 <tylercwray@gmail.com>
2026-05-29 11:21:06 -0400

    fix crash from cli config. add program-wide exception handling.

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,31 @@
 #include "UEMeta/MakeTool.hpp"
 
 int main(int argc, char** argv) {
-    if (const auto cfg_init_result = UEMeta::Config::Initialize(argc, argv)) {
-        return cfg_init_result;
+    try {
+        if (const auto cfg_init_result = UEMeta::Config::Initialize(argc, argv)) {
+            return cfg_init_result;
+        }
+
+        const auto tool = UEMeta::MakeTool();
+        if (!tool) return 0;
+
+        switch (tool->clang_tool.run(clang::tooling::newFrontendActionFactory<UEMeta::ClangHandler>().get())) {
+            case 0: {
+                std::cout << "Successfully ran tool!" << std::endl;
+            }
+            case 1: {
+                std::cout << "Failed to run tool!" << std::endl;
+            }
+            default: {
+                std::cout << "Ran tool on subset of files due to missing compile commands!" << std::endl;
+            }
+        }
+    } catch (std::exception& ex) {
+        std::cerr << std::format("Exception occurred: {}", ex.what()) << std::endl;
+        return -1;
+    } catch (...) {
+        std::cerr << std::format("Unknown exception occurred!") << std::endl;
+        return -1;
     }
-
-    const auto tool = UEMeta::MakeTool();
-    if (!tool) return 0;
-
-    switch (tool->clang_tool.run(clang::tooling::newFrontendActionFactory<UEMeta::ClangHandler>().get())) {
-        case 0: {
-            std::cout << "Successfully ran tool!" << std::endl;
-        }
-        case 1: {
-            std::cout << "Failed to run tool!" << std::endl;
-        }
-        default: {
-            std::cout << "Ran tool on subset of files due to missing compile commands!" << std::endl;
-        }
-    }
-
     return 0;
 }
