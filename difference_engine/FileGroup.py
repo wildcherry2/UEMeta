6b9a7da4e6b9d5870bf50790b0f6ffbf6e8cccc8
wildcherry2 <tylercwray@gmail.com>
2026-06-01 08:09:02 -0400

    (DE) implement FileGroup, which is a flat map of jsons

--- difference_engine/FileGroup.py
+++ difference_engine/FileGroup.py
@@ -1,7 +1,20 @@
 from pathlib import Path
+import os
 
 # Flat map of a file directory with only .json files.
 class FileGroup:
-    def __init__(self, directory: Path):
-        self.dir = directory
-        self.files: list[Path] = []
+    def __init__(self, in_directory: Path):
+        self.directory = in_directory
+        _files: list[Path] = []
+        def get_jsons(directory: Path, arr: list[Path]):
+            for root, dirs, files in os.walk(directory):
+                for file in files:
+                    if file.endswith('.json'):
+                        arr.append(Path(os.path.join(root, file)))
+                for directory in dirs:
+                    get_jsons(Path(os.path.join(root, directory)), arr)
+        get_jsons(self.directory, _files)
+        self.files: tuple[Path, ...] = tuple(_files)
+
+    def get_files(self) -> tuple[Path, ...]:
+        return self.files
