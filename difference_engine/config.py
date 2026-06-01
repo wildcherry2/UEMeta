e13961bc28ef82e36dd9c1e8dd5b3ad9ab83666f
wildcherry2 <tylercwray@gmail.com>
2026-06-01 01:28:38 -0400

    scaffold difference engine config.

---
+++ difference_engine/config.py
@@ -0,0 +1,25 @@
+import argparse
+from pathlib import Path
+
+def validate_json_dir(json_dir: str) -> str:
+    try:
+        as_path = Path(json_dir)
+        if not as_path.is_dir():
+            raise argparse.ArgumentTypeError(json_dir, f"{json_dir} is not a directory!")
+        if not any(as_path.iterdir()):
+            raise argparse.ArgumentTypeError(json_dir, f"{json_dir} is empty!")
+    except Exception as e:
+        raise argparse.ArgumentTypeError(json_dir,f"Attempting to validate {json_dir} raised exception: {e}")
+    return json_dir
+
+class Config:
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--json-dirs', type=validate_json_dir, nargs='+', help='Directories containing version-specific json files. Each directory should correspond to a single version.')
+        self.__json_dirs = parser.parse_args().json_dirs
+
+    def get_json_dirs(self):
+        return self.__json_dirs
+
+
+GLOBAL_CONFIG = Config()
