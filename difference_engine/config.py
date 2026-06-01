49c0ba84ae4f589011eac3df165a4aad9bb9cf37
wildcherry2 <tylercwray@gmail.com>
2026-06-01 07:25:27 -0400

    DE json file arg validator now makes sure there's at least 1 json file in each directory.

--- difference_engine/config.py
+++ difference_engine/config.py
@@ -1,13 +1,20 @@
 import argparse
 from pathlib import Path
+import os
 
 def validate_json_dir(json_dir: str) -> str:
     try:
         as_path = Path(json_dir)
         if not as_path.is_dir():
             raise argparse.ArgumentTypeError(json_dir, f"{json_dir} is not a directory!")
-        if not any(as_path.iterdir()):
-            raise argparse.ArgumentTypeError(json_dir, f"{json_dir} is empty!")
+        found_a_json = False
+        with os.scandir(as_path) as it:
+            for entry in it:
+                if entry.is_file() and entry.name.endswith('.json'):
+                    found_a_json = True
+                    break
+        if not found_a_json:
+            raise argparse.ArgumentTypeError(json_dir, f"{json_dir} does not contain a JSON file!")
     except Exception as e:
         raise argparse.ArgumentTypeError(json_dir,f"Attempting to validate {json_dir} raised exception: {e}")
     return json_dir
