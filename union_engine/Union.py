cdc6a98e0714c4b35374ccbb91bef5c002c01bd9
wildcherry2 <tylercwray@gmail.com>
2026-06-04 23:13:06 -0400

    (ue) simplify union ops

--- union_engine/Union.py
+++ union_engine/Union.py
@@ -7,62 +7,55 @@ from Group import NameGroup, File
 
 
 @dataclass
-class VersionedFieldInstance[T]:
+class VersionedFieldInstance[T = Any]:
     versions: set[str]
     instance: T
 
 @dataclass
-class VersionedField[T]:
+class VersionedField[T = Any]:
     instances: list[VersionedFieldInstance[T]]
+
+type UnionResult = dict[str, Any | VersionedField[Any]]
+
+def make_versioned(out: UnionResult, vfs: frozenset[str], version: str):
+    for key in out.keys():
+        if key in vfs:
+            val = out[key]
+            out[key] = VersionedField(instances=[VersionedFieldInstance(versions=set(version), instance=val)])
+
+def collapse_versioned(out: UnionResult, vfs: frozenset[str]):
+    for key in out.keys():
+        if key in vfs and len(out[key].instances) == 1:
+            out[key] = out[key].instances[0]
+
+def merge(out: UnionResult, vfs: frozenset[str], json_dict: dict[str, Any], json_version: str):
+    for vf in vfs:
+        out_field: VersionedField = out[vf]
+        current_field = json_dict[vf]
+        found = next((v for v in out_field.instances if v.instance == current_field), None)
+        if found is None:
+            out_field.instances.append(VersionedFieldInstance(instance=current_field, versions=set(json_version)))
+        else:
+            found.versions.add(json_version)
 
 # assumes all decls are EnumDeclarations and refer to the same enum across versions, and that there are at least
 # 2 files in group.values()
 def union_enums(files: list[File]):
     enum_vfs = EnumDeclaration_VF
     enumerators_vfs = Enumerator_VF
-    out: dict[str, Any | VersionedField[Any]] = files[0].get_json()
+    out: UnionResult = files[0].get_json()
 
-    # go ahead and make VersionedFields for fields that we're unioning
-    for key in out.keys():
-        if key in enum_vfs:
-            val = enum_vfs[key]
-            enum_vfs[key] = VersionedField[Any](instances=[VersionedFieldInstance[Any](versions=set(files[0].version), instance=val)])
-
-    for key in cast(dict[str, Any | VersionedField[Any]], out['enumerators']).keys():
-        if key in enumerators_vfs:
-            val = cast(dict[str, Any | VersionedField[Any]], out['enumerators'])[enumerators_vfs[key]]
-            cast(dict[str, Any | VersionedField[Any]], out['enumerators'])[enumerators_vfs[key]] \
-                = VersionedField[Any](instances=[VersionedFieldInstance[Any](versions=set(files[0].version), instance=val)])
+    make_versioned(out, enum_vfs, files[0].version)
+    make_versioned(cast(UnionResult, out['enumerators']), enumerators_vfs, files[0].version)
 
     # union files + 1 with out object
     for file in islice(files, 1, None):
         json_dict = file.get_json()
-        for evf in enum_vfs:
-            out_field: VersionedField[Any] = out[evf]
-            current_field = json_dict[evf]
-            found = next((v for v in out_field.instances if v.instance == current_field), None)
-            if found is None:
-                out_field.instances.append(VersionedFieldInstance[Any](versions=set(file.version), instance=current_field))
-            else:
-                found.versions.add(file.version)
-        for evf in enumerators_vfs:
-            out_field: VersionedField[Any] = cast(dict[str, Any | VersionedField[Any]], out['enumerators'])[evf]
-            current_field = json_dict['enumerators'][evf]
-            found = next((v for v in out_field.instances if v.instance == current_field), None)
-            if found is None:
-                out_field.instances.append(
-                    VersionedFieldInstance[Any](versions=set(file.version), instance=current_field))
-            else:
-                found.versions.add(file.version)
+        merge(out, enum_vfs, json_dict, file.version)
+        merge(cast(UnionResult, out['enumerators']), enumerators_vfs, json_dict['enumerators'], file.version)
 
-    # post-processing: if vfs have only one field, collapse them back into a normal dict entry
-    for key in out.keys():
-        if key in enum_vfs and len(cast(VersionedField[Any], out[key]).instances) == 1:
-            out[key] = cast(VersionedField[Any], out[key]).instances[0]
-
-    for key in cast(dict[str, Any | VersionedField[Any]], out['enumerators']).keys():
-        if key in enumerators_vfs and len(cast(VersionedField[Any], out['enumerators'][key]).instances) == 1:
-            out['enumerators'][key] = out['enumerators'][key].instances[0]
+    collapse_versioned(out, enum_vfs)
+    collapse_versioned(cast(UnionResult, out['enumerators']), enumerators_vfs)
 
     return out
 
